f0ed1712ba28f1bc632629f318093f32e39ec42e
felox <jimsaidsequeira@gmail.com>
2026-07-07 08:05:14 -0600

    Create initial Django backend structure

---
+++ apps/tecnicos/apps.py
@@ -0,0 +1,6 @@
+﻿from django.apps import AppConfig
+
+
+class TecnicosConfig(AppConfig):
+    name = 'apps.tecnicos'
+
---
+++ backend/urls.py
@@ -0,0 +1,30 @@
+"""
+URL configuration for backend project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('usuarios/', include('apps.usuarios.urls')),
+    path('clientes/', include('apps.clientes.urls')),
+    path('tecnicos/', include('apps.tecnicos.urls')),
+    path('servicios/', include('apps.servicios.urls')),
+    path('inventario/', include('apps.inventario.urls')),
+    path('facturacion/', include('apps.facturacion.urls')),
+    path('soporte/', include('apps.soporte.urls')),
+    path('dashboard/', include('apps.dashboard.urls')),
+]
